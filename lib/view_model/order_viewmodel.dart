e90cc2c098f8b8419962a35901370659f05a39ff
Erfan Nasri <erfannasri2@gmail.com>
2023-05-26 07:52:27 +0330

    fix(feature/Orders) : fix order viewModel

--- lib/view_model/order_viewmodel.dart
+++ lib/view_model/order_viewmodel.dart
@@ -3,13 +3,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:owner_ordering_frontend/utils/constants.dart';
 import '../model/repository/order_repository_impl.dart';
 import '../model/entity/order.dart';
+
 class OrderViewModel extends ChangeNotifier {
   var repository = OrderRepositoryImpl();
 
-  StreamController<int> total = StreamController<int>();
+  StreamController<double> totalPrice = StreamController<double>();
 
-  void getTotal(int id) async {
-    order.total = (await repository.getTotal(id));
+  void getTotal(int orderId) async {
+    totalPrice =
+        (await repository.getTotal(orderId)) as StreamController<double>;
     notifyListeners();
   }
 }
