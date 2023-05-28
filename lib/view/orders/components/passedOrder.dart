97373077b3e7caaa739274ded96d49845755634e
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2023-05-28 10:29:32 -0700

    fix(Order): fix style of passedOrder.dart

--- lib/view/orders/components/passedOrder.dart
+++ lib/view/orders/components/passedOrder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:owner_ordering_frontend/view_model/orderItem_viewmodel.dart';
 
 import '../../../model/entity/orderItem.dart';
 import '../../../model/entity/socketData.dart';
@@ -18,36 +19,42 @@ class _PassedOrderState extends State<PassedOrder> {
   @override
   void initState() {
     _orderViewModel.getTotal(3);
+    setState(() {});
     super.initState();
   }
 
   final _orderViewModel = OrderViewModel();
+  final _orderItemViewModel = OrderItemViewModel();
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Container(
-      margin: const EdgeInsets.fromLTRB(8, 4, 8, 0),
+      margin: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+      decoration: BoxDecoration(
+          border: Border.all(width: 5), borderRadius: BorderRadius.circular(5)),
+      padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            widget.socketData.order.tableNumber.toString() + "میز:",
-            textAlign: TextAlign.right,
+            "میز: " + widget.socketData.order.tableNumber.toString(),
+            textAlign: TextAlign.left,
             style: const TextStyle(
                 fontFamily: "iransans",
-                fontSize: 10,
+                fontSize: 15,
                 fontWeight: FontWeight.bold),
           ),
           Container(
+            decoration: BoxDecoration(
+                border: Border.all(), borderRadius: BorderRadius.circular(5)),
+            padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
             child: Column(
               children: [
                 ListView.builder(
                     itemCount: widget.socketData.orderItem.length,
                     shrinkWrap: true,
-                    padding: EdgeInsets.only(
-                      left: 5,
-                    ),
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
                     itemBuilder: (BuildContext context, int index) {
                       return Column(
                         children: [
@@ -56,44 +63,78 @@ class _PassedOrderState extends State<PassedOrder> {
                             children: [
                               Text(widget
                                   .socketData.orderItem[index].productTitle!),
-                              Text(widget
-                                  .socketData.orderItem[index].productUnitPrice
-                                  .toString()),
+                              Text(widget.socketData.orderItem[index]
+                                      .productUnitPrice
+                                      .toString() +
+                                  " تومان"),
                             ],
-                          ),
-                          SizedBox(
-                            height: 10,
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(widget.socketData.orderItem[index].quantity
-                                  .toString()),
+                              Text(
+                                "X" +
+                                    widget.socketData.orderItem[index].quantity
+                                        .toString(),
+                                style: const TextStyle(
+                                  backgroundColor: YellowColor,
+                                  fontFamily: "iransans",
+                                ),
+                              ),
                               Text((widget.socketData.orderItem[index]
-                                          .productUnitPrice! *
-                                      widget
-                                          .socketData.orderItem[index].quantity)
-                                  .toString()),
+                                              .productUnitPrice! *
+                                          widget.socketData.orderItem[index]
+                                              .quantity)
+                                      .toString() +
+                                  " تومان"),
                             ],
+                          ),
+                          const Divider(
+                            thickness: 1,
+                            color: BlackColor,
                           ),
                         ],
                       );
                     }),
-                Row(children: [
-                  Text(widget.socketData.order.description!),
-                ]),
-                SizedBox(
-                  height: 10,
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          const Text("توضیحات:"),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(widget.socketData.order.description!),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("مبلغ قابل پرداخت: "),
-                    Text(_orderViewModel.totalPrice.toString()),
-                  ],
+                const SizedBox(
+                  height: 10,
                 ),
               ],
             ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("مبلغ قابل پرداخت: ",
+                    style: TextStyle(
+                        fontFamily: "iransans", fontWeight: FontWeight.bold)),
+                Text(_orderViewModel.totalPrice.toString(),
+                    style: const TextStyle(
+                        fontFamily: "iransans", fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
           ),
         ],
       ),
