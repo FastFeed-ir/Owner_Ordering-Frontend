d625021c43b1329ad19aa5b1d41b2491b4a79251
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2023-05-31 12:25:56 -0700

    feature(Login&signUp): add header

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -65,6 +65,7 @@ String Tick = "assets/Tick.png";
 String Zabdar = "assets/zabdar.png";
 String RestaurantLogoDef = "assets/restarauntLogo.png";
 String WhiteLogo = "assets/logo_white.png";
+String BlackLogo = "assets/logo_black.png";
 // Fonts
 String IranSansWeb = "IranSansWeb";
 String FugazOne = "FugazOne";
